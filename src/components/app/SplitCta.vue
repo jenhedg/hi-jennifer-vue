d5952da37a290018cba02392c53e1e8c8c8c07bc
Your Name Here <jennifernhf@gmail.com>
2020-05-04 21:19:40 -0700

    card component layout, styles

--- src/components/app/SplitCta.vue
+++ src/components/app/SplitCta.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class ="contentGroup contentGroup--single">
+  <section class ="">
     <div class="site__container">
         <div class="siteContent__header">
           <h2 class="siteContent__title">A Generic Global Site Title</h2>
@@ -12,7 +12,7 @@
              <div class="contentCard__imgWrap">
               <img class="contentCard__img" src="https://centsai.com/wp-content/uploads/2018/04/Financial-literacy-programs-for-the-poor-Daye-Deura-1200x825.jpg" alt="img"/>
             </div>
-          <div class="contentCard contentCard--underContent">
+          <div class="siteContent contentCard--underContent">
             <a class="siteContent__title">
               Another Global Site Title
             </a>
@@ -20,23 +20,34 @@
               <p>Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content.</p>
             </div>
             <div class="siteContent__ctaWrapper">
-              <a href="#">Learn More</a>
+                <a class="btn btn--defSize btn--pink ctaFeature__btn" href="https://github.com/jenhedg" target="_blank">Github</a>
+            </div>
+          </div>
+          </div>
+        </div> 
+    </div>
+     <div class="site__container">
+        <div class="contentGroup__inner">
+          <div class="contentCard contentCard--horizontal">
+             <div class="contentCard__imgWrap">
+              <img class="contentCard__img" src="https://centsai.com/wp-content/uploads/2018/04/Financial-literacy-programs-for-the-poor-Daye-Deura-1200x825.jpg" alt="img"/>
+            </div>
+          <div class="siteContent contentCard--underContent">
+            <a class="siteContent__title">
+              Another Global Site Title
+            </a>
+            <div class="siteContent__description">
+              <p>Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content.</p>
+            </div>
+            <div class="siteContent__ctaWrapper">
+                <a class="btn btn--defSize btn--pink ctaFeature__btn" href="https://github.com/jenhedg" target="_blank">Github</a>
             </div>
           </div>
           </div>
         </div> 
     </div>
   </section>
-       
-        <!-- <div class="ctaFeature diagonalLines">
-          <a class="btn btn--defSize btn--pink ctaFeature__btn" href="https://github.com/jenhedg" target="_blank">Github</a>
-        </div>
-        <div class="ctaFeature leftRightLines">
-          <a class="btn btn--defSize btn--pink ctaFeature__btn" href="https://codepen.io/sparkleOven" target="_blank">Codepen</a>
-        </div>
-        <div class="ctaFeature diagonalLines">
-          <Button class="btn btn--defSize btn--pink ctaFeature__btn" label="Resume" to="/resume"/>
-        </div>-->
+  
 </template>
 
 <script>
