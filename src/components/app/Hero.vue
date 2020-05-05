1e8d46fe914de2a793673f76467981db2f7b1c90
Your Name Here <jennifernhf@gmail.com>
2020-05-05 15:36:11 -0700

    refactoring WIP, adding some content, manage card comp

--- src/components/app/Hero.vue
+++ src/components/app/Hero.vue
@@ -1,22 +1,31 @@
 <template>
-   <section class="jumbotron site__container">
-         <div class="siteContent__header">
+   <section class="hero">
+         <div class="hero__intro">
           <h1>Hi, I'm Jennifer.</h1>
-          <h2 class="siteContent__title">I'm a Web Developer in  <a
+          <h2 class="hero__title">I'm a Web Developer in  <a
               href="https://en.wikipedia.org/wiki/Tucson,_Arizona"
               target="_blank"
               style="text-decoration: underline;"
               >Tucson, Arizona.
             </a>
           </h2>
-          <div class="siteContent__copy">
-            <p>
-              I have over four years digital agency experience, where I helped clients across the globe to exceed their marketing technology goals. If you would like to learn more about me please see my Resume, explore my work, or drop me a line.
-          </p>
+          <div class="hero__contactLinks">
+            <a href="https://www.linkedin.com/in/jhedgcock/" target="_blank">LinkedIn</a>
+            <a href="https://github.com/jenhedg" target="_blank">Github</a>
+            <a href="https://codepen.io/sparkleOven" target="_blank">Codepen</a>
           </div>
-          <a href="https://www.linkedin.com/in/jhedgcock/" target="_blank">LinkedIn</a>
-          <a href="https://github.com/jenhedg" target="_blank">Github</a>
-          <a href="https://codepen.io/sparkleOven" target="_blank">Codepen</a>
+          <hr/>
+          <div class="hero__copy">
+            <p></p>
+            <p>
+              I have four years of front end web development experience in the digital marketing/advertising industry. I've worked alongside some notable brands around the world to build responsive webapps, content management systems and markting tools.
+            </p>
+            <p>
+              I'm currently excited about technologies like Vue.js, React.js, Gatsby, the JAMStack, AI/machine learning and TensorFlow, VR with A-Frame.</p> 
+            <p>I'm seeking my career next opportunity, click <a href="#">here</a> to learn more about me, or scroll down to see some of my work.</p>
+            <!-- <p>
+              I have experience with HTML, CSS/SCSS/, Javascript, jQuery, CMS, React.js, Vue.js, templating engines (Handlebars, Freemarker, Jinja), build tools and task runners (Webpack, NPM, Gulp, Grunt) and version control (Git, Mercurial).</p>-->
+          </div>
         </div>
     </section>
 </template>
@@ -26,7 +35,7 @@
 // import Button from "@/components/app/Button.vue";
 
 export default {
-  name: "Jumbotron",
+  name: "hero",
   components: {
     // Button
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,19 +4,19 @@
       <div class="contentGroup__inner">
         <ContentCard 
         thumbnail="https://centsai.com/wp-content/uploads/2018/04/Financial-literacy-programs-for-the-poor-Daye-Deura-1200x825.jpg"
-        title="See my work on Githb"
+        title="Card Title"
         copy="Lorem Ipsum dum dum doo doo. I type gibberish and thingies happen on screeny."
         href="http://www.google.com"
         btnLabel="Go!"
         />
         <ContentCard 
           thumbnail="https://centsai.com/wp-content/uploads/2018/04/Financial-literacy-programs-for-the-poor-Daye-Deura-1200x825.jpg"
-          title="Another Project"
+          title="Card Title"
           copy="Lorem Ipsum dum dum doo doo. This is a very exciting project, here are the technologies I used to make it. blah, de blah"
-          href="http://www.google.com"
-          btnLabel="View"
+          href="https://google.com"
+          btnLabel="View Site"
         />
-      </div>
+      </div> 
   </section>
 </template>
 
