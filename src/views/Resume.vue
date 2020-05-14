9bbf3931777f3d56b6793937a6ee03914870f09f
Your Name Here <jennifernhf@gmail.com>
2020-05-14 13:02:54 -0700

    mobile styles, misc styles

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,99 +1,88 @@
 <template>
   <section class="resume">
     <div class="resume__header">
-      <h1 class="resume__title">Jennifer Hedgcock</h1>
+      <h1 class="resume__name">Jennifer Hedgcock</h1>
       <div class="resume__contact">
-        <ul>
+        <ul class="contact__inner--top">
           <li>Tucson, Arizona | 520.400.2722</li>
           <li>
-            Email:
-            <a href="#" target="_blank">jennifernhf@gmail.com</a>
+            <a class="hover--grow" href="#" target="_blank">jennifernhf@gmail.com</a>
           </li>
           <li>
-            Website:
-            <a href="#" target="_blank">hi-jennifer.com</a>
+            <a class="hover--grow" href="#" target="_blank">hi-jennifer.com</a>
           </li>
         </ul>
-        <ul>
+        <ul class="contact__inner--bottom">
           <li>
             <a class="hover--grow" href="#" target="_blank">
-              <i class="hero__icon fab fa-linkedin"></i>LinkedIn
+              <i class="resume__icon fab fa-linkedin"></i>LinkedIn
             </a>
           </li>
           <li>
             <a class="hover--grow" href="#" target="_blank">
-              <i class="hero__icon fab fa-github"></i>Github
+              <i class="resume__icon fab fa-github"></i>Github
             </a>
           </li>
           <li>
             <a class="hover--grow" href="#" target="_blank">
-              <i class="hero__icon fab fa-codepen"></i>Codepen
+              <i class="resume__icon fab fa-codepen"></i>Codepen
             </a>
           </li>
         </ul>
       </div>
     </div>
-    <hr style="hr" />
+    <hr class="hr" />
     <h2 class="resume__category">Summary</h2>
-    <p>
-      Front End Web Developer with a customer service background and over 4
-      years of experience helping clients to bridge the gap between tech and
-      marketing. Experience with HTML, CSS/SCSS/, Javascript, jQuery, CMS,
-      React.js, Vue.js, templating engines (Handlebars, Freemarker, Jinja),
-      build tools and task runners (Webpack, NPM, Gulp, Grunt) and version
-      control (Git, Mercurial).
+    <p class="resume__copy">
+      Front End Web Developer with a customer service background and over 4 years of experience helping clients to bridge the gap between tech and marketing.
+      Experience with HTML, CSS/SCSS/, Javascript, jQuery, CMS, React.js, Vue.js, templating engines (Handlebars, Freemarker, Jinja), build tools and task runners (Webpack, NPM, Gulp, Grunt) and version control (Git, Mercurial).
     </p>
     <h2 class="resume__category">Experience</h2>
     <div class="resume__pos">
       <h3>Front-End Web Developer</h3>
       <h4>Crescendo Collective</h4>
       <h5>Feb 2016 - Mar 2020</h5>
-      <p>
-        Interacted daily with client-led and internal teams, including offshore
-        QA teams Programmed enterprise-level webapps, microsites and marketing
-        tools Integrated front-end with Magnolia CMS and Java backend Led
-        daily/weekly releases and deployment processes Created documentation for
-        both clients and internal teams
+      <p class="resume__copy">
+        Interacted daily with client-led and internal teams, including offshore QA teams Programmed enterprise-level webapps, microsites and marketing tools Integrated front-end with Magnolia CMS and Java backend
+        Led daily/weekly releases and deployment processes
+        Created documentation for both clients and internal teams
       </p>
     </div>
     <div class="resume__pos">
       <h3>Spanish Tutor</h3>
       <h4>Wyzant</h4>
       <h5>Jan 2015 - Dec 2015</h5>
-      <p>
-        Tutored K-12 and college students with Spanish coursework and test prep.
+      <p
+        class="resume__copy"
+      >Tutored K-12 and college students with Spanish coursework and test prep.</p>
+    </div>
+    <div class="resume__pos">
+      <h3>Weights & Measures Lab Assistant</h3>
+      <h4>GIA (Gemological Institute of America)</h4>
+      <h5>Jul 2013 - Aug 2014</h5>
+      <p class="resume__copy">
+        Processed 400+ stones daily using proprietary diagnostic technologies
+        Supported research team to develop and test diagnostic technologies, and project initiatives.
       </p>
     </div>
     <div class="resume__pos">
       <h3>Weights & Measures Lab Assistant</h3>
       <h4>GIA (Gemological Institute of America)</h4>
       <h5>Jul 2013 - Aug 2014</h5>
-      <p>
+      <p class="resume__copy">
         Processed 400+ stones daily using proprietary diagnostic technologies
-        Supported research team to develop and test diagnostic technologies, and
-        project initiatives.
-      </p>
-    </div>
-    <div class="resume__pos">
-      <h3>Weights & Measures Lab Assistant</h3>
-      <h4>GIA (Gemological Institute of America)</h4>
-      <h5>Jul 2013 - Aug 2014</h5>
-      <p>
-        Processed 400+ stones daily using proprietary diagnostic technologies
-        Supported research team to develop and test diagnostic technologies, and
-        project initiatives.
+        Supported research team to develop and test diagnostic technologies, and project initiatives.
       </p>
     </div>
     <div class="resume__pos">
       Student Advisor
-      <h3></h3>
-      Jones International University
+      <h3></h3>Jones International University
       <h4>GIA (Gemological Institute of America)</h4>
       <h5>Feb 2012 - Nov 2012</h5>
-      <p>
-        Assisted students with on-boarding procedures Maintained successful
-        student retention rates Mentored students on academic and work-life
-        success strategies
+      <p class="resume__copy">
+        Assisted students with on-boarding procedures
+        Maintained successful student retention rates
+        Mentored students on academic and work-life success strategies
       </p>
     </div>
     <h2 class="resume__category">Education</h2>
