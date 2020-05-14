745dc8feacfaf3deba6966a9e8d75459f1e017e7
Your Name Here <jennifernhf@gmail.com>
2020-05-13 18:55:48 -0700

    add header link hover styles, linting

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -16,47 +16,52 @@
         </ul>
         <ul>
           <li>
-            <a href="#" target="_blank">LinkedIn</a>
+            <a class="hover--grow" href="#" target="_blank">
+              <i class="hero__icon fab fa-linkedin"></i>LinkedIn
+            </a>
           </li>
           <li>
-            <a href="#" target="_blank">Github</a>
+            <a class="hover--grow" href="#" target="_blank">
+              <i class="hero__icon fab fa-github"></i>Github
+            </a>
           </li>
           <li>
-            <a href="#" target="_blank">Codepen</a>
+            <a class="hover--grow" href="#" target="_blank">
+              <i class="hero__icon fab fa-codepen"></i>Codepen
+            </a>
           </li>
         </ul>
       </div>
     </div>
     <hr style="hr" />
-    <h2>Summary</h2>
+    <h2 class="resume__category">Summary</h2>
     <p>
-      Front End Web Developer with a customer service background and over 4 years of experience helping clients to bridge the gap between tech and marketing.
-      Experience with HTML, CSS/SCSS/, Javascript, jQuery, CMS, React.js, Vue.js, templating engines (Handlebars, Freemarker, Jinja), build tools and task runners (Webpack, NPM, Gulp, Grunt) and version control (Git, Mercurial).
+      Front End Web Developer with a customer service background and over 4
+      years of experience helping clients to bridge the gap between tech and
+      marketing. Experience with HTML, CSS/SCSS/, Javascript, jQuery, CMS,
+      React.js, Vue.js, templating engines (Handlebars, Freemarker, Jinja),
+      build tools and task runners (Webpack, NPM, Gulp, Grunt) and version
+      control (Git, Mercurial).
     </p>
-    <h2>Experience</h2>
+    <h2 class="resume__category">Experience</h2>
     <div class="resume__pos">
       <h3>Front-End Web Developer</h3>
       <h4>Crescendo Collective</h4>
-      <h5>Feb 2016 - Mar 2020 (4 years 2 months)</h5>
+      <h5>Feb 2016 - Mar 2020</h5>
       <p>
-        Interacted daily with client-led and internal teams, including offshore QA teams Programmed enterprise-level webapps, microsites and marketing tools Integrated front-end with Magnolia CMS and Java backend
-        Led daily/weekly releases and deployment processes
-        Created documentation for both clients and internal teams
+        Interacted daily with client-led and internal teams, including offshore
+        QA teams Programmed enterprise-level webapps, microsites and marketing
+        tools Integrated front-end with Magnolia CMS and Java backend Led
+        daily/weekly releases and deployment processes Created documentation for
+        both clients and internal teams
       </p>
     </div>
     <div class="resume__pos">
       <h3>Spanish Tutor</h3>
       <h4>Wyzant</h4>
       <h5>Jan 2015 - Dec 2015</h5>
-      <p>Tutored K-12 and college students with Spanish coursework and test prep.</p>
-    </div>
-    <div class="resume__pos">
-      <h3>Weights & Measures Lab Assistant</h3>
-      <h4>GIA (Gemological Institute of America)</h4>
-      <h5>Jul 2013 - Aug 2014</h5>
       <p>
-        Processed 400+ stones daily using proprietary diagnostic technologies
-        Supported research team to develop and test diagnostic technologies, and project initiatives.
+        Tutored K-12 and college students with Spanish coursework and test prep.
       </p>
     </div>
     <div class="resume__pos">
@@ -65,21 +70,33 @@
       <h5>Jul 2013 - Aug 2014</h5>
       <p>
         Processed 400+ stones daily using proprietary diagnostic technologies
-        Supported research team to develop and test diagnostic technologies, and project initiatives.
+        Supported research team to develop and test diagnostic technologies, and
+        project initiatives.
+      </p>
+    </div>
+    <div class="resume__pos">
+      <h3>Weights & Measures Lab Assistant</h3>
+      <h4>GIA (Gemological Institute of America)</h4>
+      <h5>Jul 2013 - Aug 2014</h5>
+      <p>
+        Processed 400+ stones daily using proprietary diagnostic technologies
+        Supported research team to develop and test diagnostic technologies, and
+        project initiatives.
       </p>
     </div>
     <div class="resume__pos">
       Student Advisor
-      <h3></h3>Jones International University
+      <h3></h3>
+      Jones International University
       <h4>GIA (Gemological Institute of America)</h4>
       <h5>Feb 2012 - Nov 2012</h5>
       <p>
-        Assisted students with on-boarding procedures
-        Maintained successful student retention rates
-        Mentored students on academic and work-life success strategies
+        Assisted students with on-boarding procedures Maintained successful
+        student retention rates Mentored students on academic and work-life
+        success strategies
       </p>
     </div>
-    <h2>Education</h2>
+    <h2 class="resume__category">Education</h2>
     <div class="resume__ed">
       <h3>MiraCosta College</h3>
       <h4>AA, Web Development &amp; Design, 2015</h4>
