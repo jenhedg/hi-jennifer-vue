e13133ea618fb7cbb2f48016c2040aea3459fa10
Your Name Here <jennifernhf@gmail.com>
2020-05-14 14:35:26 -0700

    more styles, adding links, linting

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -6,25 +6,47 @@
         <ul class="contact__inner--top">
           <li>Tucson, Arizona | 520.400.2722</li>
           <li>
-            <a class="hover--grow" href="#" target="_blank">jennifernhf@gmail.com</a>
+            <a
+              class="hover--grow"
+              href="mailto:jennifernhf@gmail.com"
+              target="_blank"
+              >jennifernhf@gmail.com</a
+            >
           </li>
           <li>
-            <a class="hover--grow" href="#" target="_blank">hi-jennifer.com</a>
+            <a
+              class="hover--grow"
+              href="http://www.hi-jennifer.com"
+              target="_blank"
+              >hi-jennifer.com</a
+            >
           </li>
         </ul>
         <ul class="contact__inner--bottom">
           <li>
-            <a class="hover--grow" href="#" target="_blank">
+            <a
+              class="hover--grow"
+              href="https://www.linkedin.com/in/jhedgcock/"
+              target="_blank"
+            >
               <i class="resume__icon fab fa-linkedin"></i>LinkedIn
             </a>
           </li>
           <li>
-            <a class="hover--grow" href="#" target="_blank">
+            <a
+              class="hover--grow"
+              href="https://github.com/jenhedg"
+              target="_blank"
+            >
               <i class="resume__icon fab fa-github"></i>Github
             </a>
           </li>
           <li>
-            <a class="hover--grow" href="#" target="_blank">
+            <a
+              class="hover--grow"
+              href="https://codepen.io/sparkleOven"
+              target="_blank"
+            >
               <i class="resume__icon fab fa-codepen"></i>Codepen
             </a>
           </li>
@@ -33,9 +55,12 @@
     </div>
     <hr class="hr" />
     <h2 class="resume__category">Summary</h2>
-    <p
-      class="resume__copy"
-    >I am a Front End Web Developer with a customer service and digital marketing background. I have over 4 years of experience helping clients to bridge the gap between tech and marketing using modern web technologies.</p>
+    <p class="resume__copy">
+      I am Front End Web Developer with a self-starting customer service and
+      digital marketing background. I have over 4 years of experience helping
+      clients to bridge the gap between tech and marketing using modern web
+      technologies.
+    </p>
     <div class="resume__techlist">
       <ul class>
         <h3>Technical Experience</h3>
@@ -53,9 +78,11 @@
       </h3>
       <h5>Feb 2016 - Mar 2020</h5>
       <p class="resume__copy">
-        Worked daily with client-led and internal teams, including offshore QA teams. Programmed enterprise-level webapps, microsites and marketing tools. Integrated front-end with Magnolia CMS and Java backend.
-        Led daily/weekly releases and deployment processes.
-        Created documentation and style guides for both clients and internal teams.
+        Worked daily with client-led and internal teams, including offshore QA
+        teams. Programmed enterprise-level webapps, microsites and marketing
+        tools. Integrated front-end with Magnolia CMS and Java backend. Led
+        daily/weekly releases and deployment processes. Created documentation
+        and style guides for both clients and internal teams.
       </p>
     </div>
     <div class="resume__pos">
@@ -64,9 +91,10 @@
         <span>Wyzant</span>
       </h3>
       <h5>Jan 2015 - Dec 2015</h5>
-      <p
-        class="resume__copy"
-      >Independetanly tutored K-12 and college students with Spanish coursework and test preparation.</p>
+      <p class="resume__copy">
+        Independently tutored K-12 and college students with Spanish coursework
+        and test preparation.
+      </p>
     </div>
     <div class="resume__pos">
       <h3>
@@ -76,7 +104,8 @@
       <h5>Jul 2013 - Aug 2014</h5>
       <p class="resume__copy">
         Processed 400+ samples daily using proprietary diagnostic technologies.
-        Supported research team to develop and test diagnostic technologies, and project initiatives.
+        Supported research team to develop and test diagnostic technologies, and
+        project initiatives.
       </p>
     </div>
     <div class="resume__pos">
@@ -85,18 +114,25 @@
         <span>Jones International University</span>
       </h3>
       <h5>Jul 2013 - Aug 2014</h5>
-      <p
-        class="resume__copy"
-      >Advised and mentored students on academic for academic prep and success in an online-learning environment. Assisted student with registration, onboarding-procedures. Maintained successful student retention rates.</p>
+      <p class="resume__copy">
+        Guided and mentored prospective and incoming students on academic for
+        academic prep and success in an online-learning environment. Assisted
+        student with registration, onboarding-procedures. Successfully
+        maintained metric goals for quarterly student retention rates.
+      </p>
     </div>
     <h2 class="resume__category">Education</h2>
     <div class="resume__ed">
-      <h3>MiraCosta College</h3>
-      <h4>AA, Web Development &amp; Design, 2015</h4>
+      <h4>
+        AA, Web Development &amp; Design,
+        <span>MiraCosta College, 2015</span>
+      </h4>
     </div>
     <div class="resume__ed">
-      <h3>The University of Arizona</h3>
-      <h4>BA, Latin American Studies &amp; Political Science, 2007</h4>
+      <h4>
+        BA, Latin American Studies &amp; Political Science,
+        <span>The University of Arizona, 2007</span>
+      </h4>
     </div>
   </section>
 </template>
