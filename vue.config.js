7999e492d5543f977b93aeeb19dcf38d5ac38d5d
Your Name Here <jennifernhf@gmail.com>
2020-05-14 19:29:57 -0700

    config

--- vue.config.js
+++ vue.config.js
@@ -12,5 +12,11 @@ module.exports = {
   },
   devServer: {
     disableHostCheck: true,
-  },
+  }
 };
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/production-sub-path/'
+    : '/'
+}
