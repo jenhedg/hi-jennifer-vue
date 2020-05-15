48ec2078013a4f7a3ba8bf124efc120ede929018
Your Name Here <jennifernhf@gmail.com>
2020-05-14 18:05:45 -0700

    contact styles

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -3,49 +3,28 @@
     <h1 class="contact__title">Contact</h1>
     <ul class="contact__list">
       <li class="contact__listItem">
-        <a
-          class="hover--grow"
-          href="mailto:jennifernhf@gmail.com"
-          target="_blank"
-        >
-          <i class="fas fa-map-pin"></i>
-          Tucson, Arizona, USA
+        <a class="hover--grow" href="mailto:jennifernhf@gmail.com" target="_blank">
+          <b>Location:</b>&nbsp; Tucson, Arizona, USA
         </a>
       </li>
       <li class="contact__listItem">
-        <a
-          class="hover--grow"
-          href="mailto:jennifernhf@gmail.com"
-          target="_blank"
-        >
-          <i class="far fa-envelope"></i>jennifernhf@gmail.com
+        <a class="hover--grow" href="mailto:jennifernhf@gmail.com" target="_blank">
+          <b>Email:</b>&nbsp;jennifernhf@gmail.com
         </a>
       </li>
       <li class="contact__listItem">
-        <a
-          class="hover--grow"
-          href="https://www.linkedin.com/in/jhedgcock/"
-          target="_blank"
-        >
-          <i class="resume__icon fab fa-linkedin"></i>LinkedIn
+        <a class="hover--grow" href="https://www.linkedin.com/in/jhedgcock/" target="_blank">
+          <i class="contact__itemIcon fab fa-linkedin"></i>LinkedIn
         </a>
       </li>
       <li class="contact__listItem">
-        <a
-          class="hover--grow"
-          href="https://github.com/jenhedg"
-          target="_blank"
-        >
-          <i class="resume__icon fab fa-github"></i>Github
+        <a class="hover--grow" href="https://github.com/jenhedg" target="_blank">
+          <i class="contact__itemIcon fab fa-github"></i>Github
         </a>
       </li>
       <li class="contact__listItem">
-        <a
-          class="hover--grow"
-          href="https://codepen.io/sparkleOven"
-          target="_blank"
-        >
-          <i class="resume__icon fab fa-codepen"></i>Codepen
+        <a class="hover--grow" href="https://codepen.io/sparkleOven" target="_blank">
+          <i class="contact__itemIcon fab fa-codepen"></i>Codepen
         </a>
       </li>
     </ul>
