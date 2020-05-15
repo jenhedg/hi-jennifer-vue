40e8ef6342d1a30cd542cf1b89f3ff1c416a21bf
Your Name Here <jennifernhf@gmail.com>
2020-05-15 13:38:13 -0700

    cleaup, fix npm security risk

--- src/components/app/Hero.vue
+++ src/components/app/Hero.vue
@@ -9,16 +9,11 @@
           class="hover--grow"
           href="https://en.wikipedia.org/wiki/Tucson,_Arizona"
           target="_blank"
-          >Tucson, Arizona.</a
-        >
+        >Tucson, Arizona.</a>
       </h2>
       <Hr />
       <div class="hero__contactLinks">
-        <a
-          class="hero__icon--withLink"
-          href="https://github.com/jenhedg"
-          target="_blank"
-        >
+        <a class="hero__icon--withLink" href="https://github.com/jenhedg" target="_blank">
           <i class="hero__icon fab fa-github"></i>
           <span class="hover--grow">Github</span>
         </a>
@@ -30,11 +25,7 @@
           <i class="hero__icon fab fa-linkedin"></i>
           <span class="hover--grow">LinkedIn</span>
         </a>
-        <a
-          class="hero__icon--withLink"
-          href="https://codepen.io/sparkleOven"
-          target="_blank"
-        >
+        <a class="hero__icon--withLink" href="https://codepen.io/sparkleOven" target="_blank">
           <i class="hero__icon fab fa-codepen"></i>
           <span class="hover--grow">Codepen</span>
         </a>
