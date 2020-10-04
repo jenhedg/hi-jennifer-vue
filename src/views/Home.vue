56a99a55b717f6704bbd90e916bab54a8372c3f7
Your Name Here <jennifernhf@gmail.com>
2020-10-04 11:18:51 -0700

    updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
       <ContentCard
         thumbnail="https://i1.wp.com/storage.googleapis.com/blog-images-backup/1*wFL3csJ96lQpY0IVT9SE3w.jpeg?ssl=1"
         title="Hello, world!"
-        copy="I built this site from scratch using Vue.js, Vue CLI and Vue Router. I chose Vue because I've worked with React and I wanted to try another popular SPA framework.  Vue's rich ecosystem of NPM packages, tools and yes, documentation (because everyone loves good documentation) makes it easy-to-learn and enjoyable to work with."
+        copy="I built this site from scratch using Vue.js, Vue CLI and Vue Router. I chose Vue because I've worked with React and I wanted to try another popular SPA framework.  Vue's rich ecosystem of NPM packages, tools and yes, documentation makes it easy-to-learn and enjoyable to work with."
         href="https://github.com/jenhedg/hi-jennifer-vue"
         btnLabel="See it on Github"
       />
