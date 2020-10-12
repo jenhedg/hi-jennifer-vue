a1b8561dc5b66a98926568ac082514e66931a290
Your Name Here <jennifernhf@gmail.com>
2020-10-11 19:02:54 -0700

    updates

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -41,25 +41,21 @@
     </div>
     <hr class="hr" />
     <h2 class="resume__category">Summary</h2>
-    <p class="resume__copy">
-      I am a Front End Web Developer with over 4 years of experience in the
-      digital marketing industry, where I helped clients bridge the gap between
-      web techno, content management and marketing objectives. This experience, along with my
-      diverse work and educational background, demonstrates my ability to
-      understand problems and craft intelligent solutions while effectively
-      working with all types of people and teams.
-    </p>
+    <p
+      class="resume__copy"
+    >I have 5 years of experience planning, programming and implementing tech solutions. I have worked with companies in many industries where I have proven to be an effective collaborator on teams of all sizes. My diverse experience and background gives me the ability to analyze problems and craft effective solutions that help clients reach their goals.</p>
     <ul class="resume__techlist">
       <h3>Technical Background</h3>
       <li class="resume__techItem">HTML, CSS/SCSS/Less, Javascript, jQuery, React.js, Vue.js</li>
       <li class="resume__techItem">
-        Dynamic Templating syntaxes&mdash; Jsx, Jsp Handlebars, Freemarker,
+        Dynamic templating&mdash; Jsx, Jsp Handlebars, Freemarker,
         Jinja
       </li>
       <li class="resume__techItem">Build tools and task runners&mdash; Webpack, NPM, Gulp, Grunt</li>
       <li class="resume__techItem">Version control&mdash; Git, Mercurial</li>
       <li class="resume__techItem">Agile Development&mdash;Jira, Confluence</li>
       <li class="resume__techItem">Dev Ops&mdash; CLI, Team City, Bamboo, Vercel</li>
+      <li class="resume__techItem">CMS&mdash; Magnolia, Salesforce, Contentful, Gatsby</li>
     </ul>
     <h2 class="resume__category">Experience</h2>
     <div class="resume__pos">
@@ -68,9 +64,11 @@
         <span>PetSmart</span>
       </h3>
       <h5 class="resume__date">Aug 2020 - present</h5>
-      <p
-        class="resume__copy"
-      >Partner with business teams to create seamless web experiences that drive sales. Support, maintain and improve legacy eCommerce marketing codebase. Improve site performances and improve accessibility.</p>
+      <p class="resume__copy">
+        Partner with business teams to deliver eCommerce web experiences. Upgrade legacy codebase tech stack to improve site performance, accessibility and
+        development environment. Update and maintain marketing content in Salesforce CommerceCloud CMS. Develop and document new processes to optimize front-end workflow and
+        cross-team communication.
+      </p>
     </div>
     <div class="resume__pos">
       <h3 class="resume__title">
