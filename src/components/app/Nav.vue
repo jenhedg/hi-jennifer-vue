f741bad2d8f2371cb57ca2e7c145ad43c25566f2
Your Name Here <jennifernhf@gmail.com>
2020-10-20 18:30:55 -0700

    add toggle btn component, toggling light/dark theme

--- src/components/app/Nav.vue
+++ src/components/app/Nav.vue
@@ -2,35 +2,28 @@
   <nav class="nav">
     <ul class="navList">
       <li class="navList__link">
-        <router-link class="hover--grow" to="/" role="button" aria-label="menu"
-          >Home</router-link
-        >
+        <router-link class="hover--grow" to="/" role="button" aria-label="menu">Home</router-link>
       </li>
       <li class="navList__link">
-        <router-link
-          class="hover--grow"
-          to="/resume"
-          role="button"
-          aria-label="menu"
-          >Resume</router-link
-        >
+        <router-link class="hover--grow" to="/resume" role="button" aria-label="menu">Resume</router-link>
       </li>
       <li class="navList__link">
-        <router-link
-          class="hover--grow"
-          to="/contact"
-          role="button"
-          aria-label="menu"
-          >Contact</router-link
-        >
+        <router-link class="hover--grow" to="/contact" role="button" aria-label="menu">Contact</router-link>
       </li>
     </ul>
+    <div>
+      <ToggleThemeBtn />
+    </div>
   </nav>
 </template>
 
 <script>
+import ToggleThemeBtn from "@/components/app/ToggleThemeBtn.vue";
 export default {
   name: "Nav",
+  components: {
+    ToggleThemeBtn
+  },
   data: function () {
     return {
       state: "default",
---
+++ src/components/app/ToggleThemeBtn.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click="toggleTheme">Toggle Me!</button>
+</template>
+
+<script>
+export default {
+  name: "ToggleThemeBtn",
+  data(){
+    return {
+      theme:'light'
+      }
+  },
+  watch: {
+    theme(theme) {
+      document.documentElement.dataset.theme = theme // this is where the magic happens
+    } // the `dataset.theme` is the data-theme attribute I set in my SCSS file (highlighted above)
+  },
+  methods:{
+     toggleTheme() {
+      this.theme = this.theme === 'light' ? 'dark' : 'light' // switch themes based on current value of `this.theme`
+    }
+  }
+  
+};
+</script>
+
+<style lang="scss">
+@import "@/styles/main.scss";
+</style>
